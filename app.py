e95370510f97aef599677b79564da390a6b784bd
Prabaharan-nagappan <prabaharan.nagappan@gmail.com>
2023-11-04 06:10:16 +0000

    sample

--- app.py
+++ app.py
@@ -4,14 +4,24 @@ from motor.motor_asyncio import AsyncIOMotorClient
 
 app = FastAPI()
 
-client = AsyncIOMotorClient("mongodb://mongo:27017")
+client = AsyncIOMotorClient("mongodb://localhost:27017")
 database = client["mydatabase"]
 collection = database["mycollection"]
 
-@app.post("/items/")
-async def create_item(item: dict):
-    await collection.insert_one(item)
-    return item
+class Item:
+    def __init__(self, name, description):
+        self.name = name
+        self.description = description
+
+@app.post("/items/", response_model=None)
+async def create_item(item: Item):
+    document = {
+        "name": item.name,
+        "description": item.description
+    }
+    result = await collection.insert_one(document)
+    item_id = str(result.inserted_id)
+    return {"item_id": item_id, **document}
 
 @app.get("/items/")
 async def read_items():
