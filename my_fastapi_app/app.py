6dd5bb9faf2a8f790a99a64948c9731d57eb6c41
Prabaharan-nagappan <prabaharan.nagappan@gmail.com>
2023-11-04 07:14:02 +0000

    modified code

--- my_fastapi_app/app.py
+++ my_fastapi_app/app.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+import pymongo
 
 app = FastAPI()
+
+# Define your MongoDB connection parameters
+mongo_client = pymongo.MongoClient("mongodb://localhost:27017/")
+db = mongo_client["mydatabase"]
 
 class Item(BaseModel):
     name: str
@@ -9,4 +14,9 @@ class Item(BaseModel):
 
 @app.post("/items/", response_model=Item)
 async def create_item(item: Item):
-    return item
+    # Insert the item into the MongoDB collection
+    item_data = item.dict()
+    result = db.items.insert_one(item_data)
+    item_id = str(result.inserted_id)
+    
+    return {"item_id": item_id, **item.dict()}
