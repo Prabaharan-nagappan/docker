1eed66c99070a5e727ac3588c97a899dfdccc10e
Prabaharan-nagappan <prabaharan.nagappan@gmail.com>
2023-11-04 05:48:38 +0000

    curd operation for fastapi

--- app.py
+++ app.py
@@ -1,8 +1,41 @@
 # app.py
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+from typing import List, Optional
 
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return {"message": "Hello, World!"}
+class Task(BaseModel):
+    title: str
+    description: Optional[str]
+
+tasks = []
+
+@app.post("/tasks/", response_model=Task)
+def create_task(task: Task):
+    tasks.append(task)
+    return task
+
+@app.get("/tasks/", response_model=List[Task])
+def read_tasks():
+    return tasks
+
+@app.get("/tasks/{task_id}", response_model=Task)
+def read_task(task_id: int):
+    if task_id < 0 or task_id >= len(tasks):
+        raise HTTPException(status_code=404, detail="Task not found")
+    return tasks[task_id]
+
+@app.put("/tasks/{task_id}", response_model=Task)
+def update_task(task_id: int, task: Task):
+    if task_id < 0 or task_id >= len(tasks):
+        raise HTTPException(status_code=404, detail="Task not found")
+    tasks[task_id] = task
+    return task
+
+@app.delete("/tasks/{task_id}", response_model=Task)
+def delete_task(task_id: int):
+    if task_id < 0 or task_id >= len(tasks):
+        raise HTTPException(status_code=404, detail="Task not found")
+    deleted_task = tasks.pop(task_id)
+    return deleted_task
